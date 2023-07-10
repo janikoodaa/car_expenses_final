424ba4381cdcf5cf7cbe40f57bbd84a4740e5b51
Jani Kaikkonen <janikaikkonen@gmail.com>
2023-07-10 15:13:32 +0300

    fixed status "light" position

--- app/library/uiComponents/vehicleCard.tsx
+++ app/library/uiComponents/vehicleCard.tsx
@@ -5,12 +5,14 @@ import { IVehicle } from "../models/Vehicle";
 
 export async function VehicleCard({ vehicle }: { vehicle: IVehicle }): Promise<JSX.Element> {
      return (
-          <div className=" flex h-52 flex-col rounded-md border-2 border-slate-200 bg-slate-300">
-               <h2 className="h-10 w-full pt-2 text-center font-bold">
-                    {vehicle.make} {vehicle.model}, {vehicle.registerNumber}
-               </h2>
+          <div className="flex h-52 flex-col rounded-md border-2 border-slate-200 bg-slate-300">
+               <div className="flex flex-row">
+                    <h2 className="h-10 w-full pt-2 text-center font-bold">
+                         {vehicle.make} {vehicle.model}, {vehicle.registerNumber}
+                    </h2>
+                    <GrStatusGoodSmall className={"ml-auto mr-1 mt-1" + (vehicle.active ? " text-green-500" : " text-gray-500")} />
+               </div>
                <h6 className="text-center text-xs">vm.{vehicle.year}, ml.viimeisimmän tankkauksen km</h6>
-               <GrStatusGoodSmall className={"absolute right-1 top-1" + (vehicle.active ? " text-green-500" : " text-gray-500")} />
                <div className="flex h-36 w-full flex-row items-center justify-center">
                     <Link
                          className="flex h-4/5 w-3/4 justify-center"
